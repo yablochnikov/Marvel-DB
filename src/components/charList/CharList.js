d137bcd95d861785def794e322c5735a1388e73b
Nikita Yablochnikov <yablochnikovnikita@gmail.com>
2022-07-16 10:29:05 +0300

    finished first page

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,123 @@
-import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import { Component } from "react";
+import PropTypes from "prop-types";
+import "./charList.scss";
+import MarvelService from "../../services/MarvelService";
+import ErrorMessage from "../errorMessage/errorMessage";
+import Spinner from "../spinner/spinner";
 
-const CharList = () => {
+class CharList extends Component {
+  state = {
+    charList: [],
+    loading: true,
+    error: false,
+    newItemsLoading: false,
+    offset: 0,
+    charsEnded: false
+  };
+
+  marvelService = new MarvelService();
+
+  componentDidMount = () => {
+    this.marvelService
+      .getAllCharacters()
+      .then(this.onCharLisLoaded)
+      .catch(this.onError);
+  };
+
+  onRequest = offset => {
+    this.onMoreCharactersLoading();
+    this.marvelService
+      .getAllCharacters(offset)
+      .then(this.onMoreCharactersLoaded)
+      .catch(this.onError);
+    console.log(this.state);
+  };
+
+  onMoreCharactersLoading = () => {
+    this.setState({
+      newItemsLoading: true
+    });
+  };
+
+  onMoreCharactersLoaded = newCharList => {
+    let ended = false;
+    if (newCharList.lenght < 9) {
+      ended = true;
+    }
+
+    this.setState({
+      charList: [...this.state.charList, ...newCharList],
+      loading: false,
+      newItemsLoading: false,
+      offset: 9 + this.state.offset,
+      charEnded: ended
+    });
+  };
+
+  onCharLisLoaded = charList => {
+    this.setState({ charList, loading: false });
+  };
+
+  onError = () => {
+    this.setState({ loading: false, error: true });
+  };
+
+  renderItems(arr) {
+    const items = arr.map(item => {
+      let imgStyle = { objectFit: "cover" };
+      if (
+        item.thumbnail ===
+        "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+      ) {
+        imgStyle = { objectFit: "unset" };
+      }
+      return (
+        <li
+          className='char__item'
+          key={item.id}
+          onClick={() => this.props.onCharSelected(item.id)}
+        >
+          <img src={item.thumbnail} alt={item.name} style={imgStyle} />
+          <div className='char__name'>{item.name}</div>
+        </li>
+      );
+    });
+
+    return <ul className='char__grid'>{items}</ul>;
+  }
+
+  render() {
+    const { charList, loading, error, newItemsLoading, offset, charsEnded } =
+      this.state;
+    const items = this.renderItems(charList);
+
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? items : null;
     return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+      <div className='char__list'>
+        <ul className='char__grid'>
+          {errorMessage}
+          {spinner}
+          {content}
+        </ul>
+        <button
+          style={{ display: charsEnded ? "none" : "block" }}
+          className='button button__main button__long'
+          onClick={() => {
+            this.onRequest(offset + 9);
+          }}
+          disabled={newItemsLoading}
+        >
+          <div className='inner'>load more</div>
+        </button>
+      </div>
+    );
+  }
 }
+
+CharList.propTypes = {
+  onCharSelected: PropTypes.func.isRequired
+};
 
 export default CharList;
---
+++ src/services/MarvelService.js
@@ -0,0 +1,43 @@
+class MarvelService {
+  _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  _apiKey = "apikey=8d7ca5b95586291c6bf22ecc9918f8b8";
+  _baseOffset = 0;
+
+  getResource = async url => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  getAllCharacters = async (offset = this._baseOffset) => {
+    const res = await this.getResource(
+      `${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`
+    );
+    return res.data.results.map(this._transformCharacter);
+  };
+
+  getCharacter = async id => {
+    const res = await this.getResource(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    );
+    return this._transformCharacter(res.data.results[0]);
+  };
+
+  _transformCharacter = char => {
+    return {
+      id: char.id,
+      name: char.name,
+      description: char.description,
+      thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+      comics: char.comics.items
+    };
+  };
+}
+
+export default MarvelService;
